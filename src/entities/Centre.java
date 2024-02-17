5333d4b6068d1e3682a37d67c23509c0bdf520ca
hannahomeiza <omeiza81@gmail.com>
2024-02-17 13:57:43 -0500

    centre

---
+++ src/entities/Centre.java
@@ -0,0 +1,16 @@
+package entities;
+
+public class Centre {
+
+    public String Centre_name;
+    public String centre_type;
+    public String location;
+
+    public Centre(String name, String centre_type, String location){
+        this.Centre_name = name;
+        this.centre_type = centre_type;
+        this.location = location;
+    }
+
+
+}
