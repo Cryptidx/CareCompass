a06d7771f7be5a2696e6f3903e4c8c9ac115fe0b
hannahomeiza <omeiza81@gmail.com>
2024-02-17 15:37:04 -0500

    more entites

--- src/entities/Centre_log.java
+++ src/entities/Centre_log.java
@@ -9,4 +9,6 @@ public class Centre_log {
     // this is for shelters in need actually. but could be extended to volunteer communities
     public Map<String,Integer> centres_in_need; // it can map to anything tbh,
     // however, we want to use map so removing it is constant time
+
+
 }
